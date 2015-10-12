e2f8d1b9e7d40e240760bc03cbc58d6f43bc0231
sandyukraine <Oleksandr_Kohut@epam.com>
2015-10-12 17:48:07 +0200

    some fixing

--- main/DependenciesCalculator.py
+++ main/DependenciesCalculator.py
@@ -3,12 +3,10 @@ __author__ = 'Oleksandr_Kohut'
 class DependenciesCalculator():
 
     def __init__(self):
-        self.dependencies = []
+        self.dependencies = {}
 
     def add_direct(self, item, dependencies):
-            for item in dependencies:
-                self.dependencies[item, dependencies]
+        self.dependencies.update({item: dependencies})
 
     def dependencies_for(self, item):
-        for item in self.dependencies:
-            return item[1]
+        return self.dependencies.get(item)
