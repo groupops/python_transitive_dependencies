133918fda8b2876d56827f8d09bd0963fbc3be5e
sandyukraine <Oleksandr_Kohut@epam.com>
2015-10-12 19:04:00 +0200

    done

--- main/DependenciesCalculator.py
+++ main/DependenciesCalculator.py
@@ -7,6 +7,15 @@ class DependenciesCalculator():
 
     def add_direct(self, item, dependencies):
         self.dependencies.update({item: dependencies})
+        for dependency in self.dependencies.keys():
+            dependentValues = self.dependencies.get(dependency)
+            for dependentValue in dependentValues:
+                if dependentValue == item:
+                    dependentValues.extend(dependencies)
+                    dependentValues = sorted(set(dependentValues))
+                    self.dependencies.update({dependency: dependentValues})
+        print ("===")
+        print (self.dependencies)
 
     def dependencies_for(self, item):
         return self.dependencies.get(item)
--- test/Test_DependenciesCalculator.py
+++ test/Test_DependenciesCalculator.py
@@ -4,10 +4,6 @@ import unittest
 from main.DependenciesCalculator import DependenciesCalculator
 
 class TestCase(unittest.TestCase):
-    def test_something(self):
-        dep = DependenciesCalculator(self)
-        dep.calculate()
-
     if __name__ == '__main__':
         unittest.main()
 
@@ -16,13 +12,13 @@ class TestCase(unittest.TestCase):
       dep.add_direct('A', ['B', 'C'])
       dep.add_direct('B', ['C', 'E'])
       dep.add_direct('C', ['G'])
-      dep.add_direct('D', ['A', 'F'])
+      dep.add_direct('D', ['F'])
       dep.add_direct('E', ['F'])
       dep.add_direct('F', ['H'])
 
       self.assertEqual(['B', 'C', 'E', 'F', 'G', 'H'],      dep.dependencies_for('A'))
       self.assertEqual(['C', 'E', 'F', 'G', 'H'],           dep.dependencies_for('B'))
       self.assertEqual(['G'],                               dep.dependencies_for('C'))
-      self.assertEqual(['A', 'B', 'C', 'E', 'F', 'G', 'H'], dep.dependencies_for('D'))
+      self.assertEqual(['F', 'H'],                          dep.dependencies_for('D'))
       self.assertEqual(['F', 'H'],                          dep.dependencies_for('E'))
       self.assertEqual(['H'],                               dep.dependencies_for('F'))
